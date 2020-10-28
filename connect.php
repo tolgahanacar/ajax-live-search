1d247aea2fabf2ee158f692175dad6fd73241284
Tolgahan ACAR <47753083+tolgahanacar@users.noreply.github.com>
2020-10-28 20:16:21 +0300

    Update connect.php
    Update connect.php -> Connect to database settings

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $db = new PDO("mysql:host=localhost;dbname=livesearch;charset=UTF8","tolga","123456");//write your database name correctly
+    $db = new PDO("mysql:host=localhost;dbname=livesearch;charset=UTF8","root","");//write your database name correctly
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch (PDOException $e){
     echo $e->getMessage();
