4116c477722178b3db8a0fc6cc6a82ab0840be63
Tolgahan <47753083+tolgahanacar@users.noreply.github.com>
2024-08-15 10:27:30 +0300

    Update index.php
    jQuery CDN Update: The smaller min.js version was used for reduced file size.
    
    Optimized on("input") Event: The input event is used instead of keyup, allowing non-keyboard inputs (e.g., copy-paste) to be handled.
    
    Simplified jQuery $.post Function: The $.post function is used for a more concise and readable Ajax request.
    
    Visual Enhancement: A placeholder was added to the input element to better indicate the search functionality to users.
    
    Minify: Unnecessary code and spaces were removed, making the code more compact.

--- index.php
+++ index.php
@@ -3,39 +3,32 @@ require_once 'connect.php';
 ?>
 
 <!doctype html>
-<html lang="tr-TR">
+<html lang="tr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
     <script type="text/javascript">
-        $(document).ready(function(){
-            $("input[name=sea]").keyup(function(){
-                var value = $(this).val();
-                var sea = "value="+value;
-                $.ajax({
-                    type:"POST",
-                    url:"ajax.php",
-                    data: sea,
-                    success: function(sonuc){
-                        $("#result").show().html(sonuc);
-                    }
+        $(document).ready(function() {
+            $("#sea").on("input", function() {
+                $.post("ajax.php", { value: $(this).val() }, function(response) {
+                    $("#result").html(response).show();
                 });
             });
         });
     </script>
 </head>
 <body>
-<form action="">
-    <div>
-        <input type="search" name="sea" id="sea">
-    </div>
-    <div>
-       <p id="result"></p>
-    </div>
-</form>
+    <form>
+        <div>
+            <input type="search" name="sea" id="sea" placeholder="Search...">
+        </div>
+        <div>
+            <p id="result"></p>
+        </div>
+    </form>
 </body>
 </html>
