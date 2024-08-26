67a04d6c44e54d2424562b164b839ccc72cdb1d5
Tolgahan <47753083+tolgahanacar@users.noreply.github.com>
2024-08-26 12:55:57 +0300

    Update index.php
    Content Security Policy:
    Added a Content-Security-Policy header for better security.
    Debouncing:
    Added a debounce mechanism to delay the AJAX request until the user stops typing for 300ms.
    Form Method:
    Added method="post" to the <form> tag to prevent default form submission.
    HTML Semantics:
    Minor improvements for better structure.

--- index.php
+++ index.php
@@ -8,21 +8,27 @@ require_once 'connect.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' https://code.jquery.com; style-src 'self'; img-src 'self'">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
     <script type="text/javascript">
         $(document).ready(function() {
+            let timeout = null;
+            
             $("#sea").on("input", function() {
-                $.post("ajax.php", { value: $(this).val() }, function(response) {
-                    $("#result").html(response).show();
-                });
+                clearTimeout(timeout);
+                timeout = setTimeout(function() {
+                    $.post("ajax.php", { value: $("#sea").val() }, function(response) {
+                        $("#result").html(response).show();
+                    });
+                }, 300); // Delay of 300ms
             });
         });
     </script>
 </head>
 <body>
-    <form>
+    <form method="post" action="#">
         <div>
             <input type="search" name="sea" id="sea" placeholder="Search...">
         </div>
